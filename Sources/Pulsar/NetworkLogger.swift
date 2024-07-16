6fa4fa8d971cc1d7d6a347823c5f96d02680a49a
Dmitry Maslennikov <mrmda28@gmail.com>
2024-07-16 12:27:15 +0300

    Add comments for the prepare method

--- Sources/Pulsar/NetworkLogger.swift
+++ Sources/Pulsar/NetworkLogger.swift
@@ -34,6 +34,30 @@ public final class NetworkLogger {
     
     private init() { }
     
+    /// Prepares the logger for use. This method should be called in the AppDelegate's didFinishLaunchingWithOptions method.
+    ///
+    /// - Parameter forNative: A boolean flag that indicates whether the requests are performed
+    ///   using a native framework. If `true`, the logger assumes that the requests are executed
+    ///   via native frameworks (e.g., URLSession in iOS). If `false`, it indicates that the
+    ///   requests are executed via non-native frameworks (e.g., Ktor from Kotlin Multiplatform).
+    ///   When set to `false`, additional setup is performed to enable logging for non-native
+    ///   frameworks. Defaults to `true`.
+    ///
+    /// This method ensures that the logger is prepared only once. If the logger is already prepared,
+    /// subsequent calls to this method have no effect.
+    ///
+    /// - Note: When using a non-native framework like Ktor from Kotlin Multiplatform, set the
+    ///   `forNative` parameter to `false` to enable automatic registration and proxy setup
+    ///   required for proper logging.
+    ///
+    /// Example usage:
+    /// ```
+    /// // For native frameworks (e.g., URLSession)
+    /// NetworkLogger.shared.prepare()
+    ///
+    /// // For non-native frameworks (e.g., Ktor)
+    /// NetworkLogger.shared.prepare(forNative: false)
+    /// ```
     public func prepare(forNative: Bool = true) {
         guard !isPrepared else { return }
         
